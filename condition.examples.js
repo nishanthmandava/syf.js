3a780bab1e6fce0ac9f50547e9eef9b65de44e46
nishanthmandava <nishanth.mandava@syf.com>
2021-04-05 11:44:34 +0000

    condition
    Signed-off-by: nishanthmandava <nishanth.mandava@syf.com>

---
+++ condition.examples.js
@@ -0,0 +1,38 @@
+// find the given number is odd or even
+n = 11
+console.log(n%2);
+
+console.log(n%2==0);
+
+if(n%2==1){ //if block
+    console.log("odd number");
+}
+else{ //else block
+    console.log("even number");
+}
+
+// + : adition
+// - : sub
+// * : mul
+// / : div
+// % : mod
+// = : assign
+// == : compare data
+// === : compare data and type
+// < : less than
+// > : greater than
+
+//find which number is bigger
+
+let x = 20;
+let y = 20;
+
+if (x>y){
+    console.log("x is greater");
+}else if (y>x){
+    console.log("y is greater");
+}else {
+    console.log("both are equal");
+}
+
+
--- demo.js
+++ demo.js
@@ -1,1 +1,35 @@
 
+//"use strict"//force user to declare variable 
+//let assign value to x
+
+
+console.log("Hello")
+
+
+x=20
+y=90
+z=x+y
+console.log(z)
+
+x=500
+y=600
+z=x-y
+console.log(z)
+
+//array's example
+
+let userdata = ["syf",4,2021];
+console.log(userdata[0]);
+console.log(userdata[1]);
+console.log(userdata[2]);
+
+userdata.push(10,20,30);
+console.log(userdata);
+
+
+
+
+
+
+
+
