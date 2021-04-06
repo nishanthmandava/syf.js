e9e0eb114a013ad384a357a7f21749e2a243b64e
nishanthmandava <nishanth.mandava@syf.com>
2021-04-06 12:13:05 +0000

    nnnn

--- assignments.js
+++ assignments.js
@@ -1,7 +1,7 @@
 //find factors of a given number e.g 20 -  1 2 4 5 10 20
-let n = 20
+let n = 25
 
-for(let i=1;i<=20; i++){
+for(let i=1;i<=25; i++){
     if (n%i==0){
         console.log(i);
         
@@ -9,9 +9,9 @@ for(let i=1;i<=20; i++){
 }
 //find factorial of a given number e.g 5 - 5*4*3*2*1
 
-let n1 = 5;
+let n1 = 2;
 let res = 1;
-for(let i=1;i<=5; i++){
+for(let i=1;i<=n1; i++){
     res = res*i
     console.log(res);
     
---
+++ function.js
@@ -0,0 +1,15 @@
+// as function is a reusable code block
+
+function findodd(n) {
+
+if (n%2==1){
+    console.log(n+ "is a odd number");
+    
+}else {
+    console.log(n+"is a even number");
+}
+}
+
+findodd(10)
+findodd(20)
+findodd(2238)
